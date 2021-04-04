404350fdcb7e57a0692dab7ccef831ada702a5d1
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-04-04 11:33:21 +0600

    update

--- src/server.js
+++ src/server.js
@@ -1,23 +1,24 @@
 require("dotenv").config();
 import express from "express";
-import viewEngine from "./config/viewEngine";
-import initWebRoute from "./routes/web";
+import configViewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
 import bodyParser from "body-parser";
+
 
 let app = express();
 
-// config view engine
-viewEngine(app);
-
-//use body-parser to post data
+//config body-parser to post data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// init all web routes
-initWebRoute(app);
+//config view engine
+configViewEngine(app);
+
+//init web routes
+initWebRoutes(app);
 
 let port = process.env.PORT || 8080;
 
-app.listen(port, ()=>{
-   console.log(`App is running at the port ${port}`) ;
+app.listen(port, () => {
+    console.log(`Messenger tech shop is running at the port ${port}`);
 });
